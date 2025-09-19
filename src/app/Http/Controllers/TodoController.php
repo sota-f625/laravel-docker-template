eb36267dc9c9512a65627fbe46feab8152a7fdb6
sota-f625 <s7716xf625@gmail.com>
2025-09-19 16:40:26 +0900

    feat: 新規作成画面実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -15,5 +15,12 @@ class TodoController extends Controller
 
         return view('todo.index', ['todos' => $todos]);
     }
-}
 
+    public function create()
+    {
+
+        return view('todo.create');
+    }
+
+
+}
