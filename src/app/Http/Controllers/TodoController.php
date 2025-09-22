7a78a422c962f0ae664801bec0acb68660904504
sota-f625 <s7716xf625@gmail.com>
2025-09-22 14:24:05 +0900

    feat: 編集画面表示

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -48,4 +48,10 @@ class TodoController extends Controller
         return view('todo.show', ['todo' => $todo]);
     }
 
+    public function edit($id)
+    {
+        $todo = $this->todo->find($id);
+
+        return view('todo.edit', ['todo' => $todo]);
+    }
 }
